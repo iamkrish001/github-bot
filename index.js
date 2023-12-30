1b3c5a37ce644d074ba6871151d989622932f514
Krish Bhurtel <bhurtelkrish123@gmail.com>
2023-12-30 17:42:04 +0545

    dfgfd

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-console.log("krish")
-console.log("bhurtel")
-console.log("bhurtel")
+const jsonfile = require('jsonfile' ) ;
+const moment = require('moment') ;
+const simpleGit = require('simple-git') ;
+const FILE_PATH ='./data.json' ;
+const DATE = moment().subtract(1,'d').format() ;
+
+const data = {
+    date : DATE 
+}
+jsonfile.writeFile(FILE_PATH, data)
+
+simpleGit().add([FILE_PATH]).commit(DATE, { '--date': DATE }).push() ;  
